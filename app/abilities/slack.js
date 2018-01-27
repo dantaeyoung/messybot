5b89ea9c7a9635d876c1772c150509e38fbdaee8
dantaeyoung <dan@dantaeyoung.com>
2018-01-26 19:24:50 -0500

    slackbot autoreconnects

--- app/abilities/slack.js
+++ app/abilities/slack.js
@@ -1,13 +1,36 @@
 
+
+
 class slack {
+
   constructor(config) {
-    // CONNECT SLACK
+   // CONNECT SLACK
     var Botkit = require('botkit');
+
     this.controller = Botkit.slackbot();
     this.bot = this.controller.spawn({
         token: config.slack.api_token
-    }).startRTM();
+    });
+
+		var self = this;
+		this.controller.on('rtm_close', function(bot, err) {
+				self.start_rtm();
+		});
+
+    this.start_rtm();
   }
+
+	start_rtm() {
+		var self = this;
+    this.bot.startRTM(function(err,bot,payload) {
+			if (err) {
+					console.log('Failed to start RTM')
+					return setTimeout(self.start_rtm, 60000);
+			}
+			console.log("RTM started!");
+		});
+	}
+
 }
 
 module.exports = slack;
