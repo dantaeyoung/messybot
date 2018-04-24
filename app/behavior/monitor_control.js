a2036aa785f3581711cdc2a3c5809ca1dc172033
Dan Taeyoung <provolot@gmail.com>
2018-04-23 23:14:29 -0400

    adding monitor control ability

---
+++ app/behavior/monitor_control.js
@@ -0,0 +1,36 @@
+module.exports = function(config, abilities) { 
+
+  var controller = abilities.slack.controller;
+
+  function monitorOff() {
+      bot.reply(message,'Okay! Turning monitor off!');
+      cmd.get('vcgencmd display_power 0', function(err, data, stderr) {
+        //console.log('the current working dir is : ',data)
+      });
+  });
+  function monitorOn() {
+      bot.reply(message,'Okay! Turning monitor on!');
+      cmd.get('vcgencmd display_power 1', function(err, data, stderr) {
+        //console.log('the current working dir is : ',data)
+      });
+  });
+
+  controller.hears('turn monitor (.*)',['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    if(message.match[1] == "off") {
+      monitorOff();
+    } else {
+      monitorOn();
+    }
+  });
+
+  controller.hears(['on', 'off'],['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    if(message.match[0] == "off") {
+      monitorOff();
+    } else {
+      monitorOn();
+    }
+  });
+
+
+}
+
