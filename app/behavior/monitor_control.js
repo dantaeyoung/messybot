286561636ff0b7eec77a370e572277f3c15d5002
Dan Taeyoung <provolot@gmail.com>
2018-04-23 23:18:20 -0400

    monitor control working

--- app/behavior/monitor_control.js
+++ app/behavior/monitor_control.js
@@ -1,15 +1,15 @@
+var cmd = require('node-cmd');
+
 module.exports = function(config, abilities) { 
 
   var controller = abilities.slack.controller;
 
   function monitorOff() {
-      bot.reply(message,'Okay! Turning monitor off!');
       cmd.get('vcgencmd display_power 0', function(err, data, stderr) {
         //console.log('the current working dir is : ',data)
       });
   };
   function monitorOn() {
-      bot.reply(message,'Okay! Turning monitor on!');
       cmd.get('vcgencmd display_power 1', function(err, data, stderr) {
         //console.log('the current working dir is : ',data)
       });
@@ -17,16 +17,20 @@ module.exports = function(config, abilities) {
 
   controller.hears('turn monitor (.*)',['direct_message', 'direct_mention', 'mention'], function(bot, message) {
     if(message.match[1] == "off") {
+      bot.reply(message,'Okay! Turning monitor off!');
       monitorOff();
     } else {
+      bot.reply(message,'Okay! Turning monitor on!');
       monitorOn();
     }
   });
 
   controller.hears(['on', 'off'],['direct_message', 'direct_mention', 'mention'], function(bot, message) {
     if(message.match[0] == "off") {
+      bot.reply(message,'Okay! Turning monitor off!');
       monitorOff();
     } else {
+      bot.reply(message,'Okay! Turning monitor on!');
       monitorOn();
     }
   });
