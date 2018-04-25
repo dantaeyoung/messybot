a8aea28f780b910eb89b796661c3bbebd0261b89
dan taeyoung <dan@dantaeyoung.com>
2018-04-25 14:00:09 -0400

    Adding banter

--- app/behavior/banter.js
+++ app/behavior/banter.js
@@ -6,5 +6,17 @@ module.exports = function(config, abilities) {
     bot.reply(message,'Hello yourself.');
   });
 
+  controller.hears(['what can you do'],['direct_message','direct_mention','mention'],function(bot,message) {
+    bot.reply(message,"Not that much right now, I'm afraid.");
+  });
+
+  controller.hears(['lunch', 'dinner'],['direct_message','direct_mention','mention'],function(bot,message) {
+    bot.reply(message,"I'm not hungry because I don't have a stomach, but you go ahead and eat without me.");
+  });
+
+  controller.hears(['good bot'],['direct_message','direct_mention','mention'],function(bot,message) {
+    bot.reply(message,"awwww thank you!.");
+  });
+
 }
 
