64d8c416d41c9e0ed3a51ec28b349d6890c2c517
Dan Taeyoung <provolot@gmail.com>
2018-05-17 17:07:13 -0400

    weather digits

--- app/behavior/ask_about_the_weather.js
+++ app/behavior/ask_about_the_weather.js
@@ -7,8 +7,7 @@ module.exports = function(config, abilities) {
 
     abilities.ambientweather.get_weather_slow((d) => {
 
-      var human_message = `Hi! Hope you're having a nice day! The wind is blowing at ${d.winddir} degrees from North at ${d.windspeedmph}mph! Outside, it's ${d.tempf} degrees but feels like ${d.feelsLike}F with a humidity of ${d.humidity}. Inside, it's ${d.tempinf} degrees. The solar radiation is ${d.solarradiation} and it's rained ${d.dailyrainin}in today so far.`
-        
+      var human_message = `Hi! The wind is blowing at ${d.winddir} degrees from North at ${d.windspeedmph}mph! Outside, it's ${d.tempf} degrees but feels like ${d.feelsLike.toFixed(2)} with a humidity of ${d.humidity}. Inside, it's ${d.tempinf} degrees. The solar radiation is ${d.solarradiation} and it's rained ${d.dailyrainin}in today so far. Hope you're having a nice day!` 
       bot.reply(message, human_message);
 
     });
