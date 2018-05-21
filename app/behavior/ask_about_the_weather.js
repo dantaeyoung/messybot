22d70852085e05eec61f3ad72591b857cdb0e7da
Dan Taeyoung <provolot@gmail.com>
2018-05-21 16:41:04 -0400

    Adding cardinal directions to weather info

--- app/behavior/ask_about_the_weather.js
+++ app/behavior/ask_about_the_weather.js
@@ -1,3 +1,8 @@
+function degToCompass(num) {
+    var val = Math.floor((num / 22.5) + 0.5);
+    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    return arr[(val % 16)];
+}
 
 module.exports = function(config, abilities) { 
 
@@ -7,7 +12,9 @@ module.exports = function(config, abilities) {
 
     abilities.ambientweather.get_weather_slow((d) => {
 
-      var human_message = `Hi! The wind is blowing at ${d.winddir} degrees from North at ${d.windspeedmph}mph! Outside, it's ${d.tempf} degrees but feels like ${d.feelsLike.toFixed(2)} with a humidity of ${d.humidity}. Inside, it's ${d.tempinf} degrees. The solar radiation is ${d.solarradiation} and it's rained ${d.dailyrainin}in today so far. Hope you're having a nice day!` 
+      var compass = degToCompass(d.winddir);
+
+      var human_message = `Hi! The wind is blowing from ${compass} at ${d.windspeedmph}mph! Outside, it's ${d.tempf} degrees but feels like ${d.feelsLike.toFixed(2)} with a humidity of ${d.humidity}. Inside, it's ${d.tempinf} degrees. The solar radiation is ${d.solarradiation} and it's rained ${d.dailyrainin}in today so far. Hope you're having a nice day!` 
       bot.reply(message, human_message);
 
     });
