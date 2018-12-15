eaca52e5a8d760903f128cc26a9b7eed14f326a5
Dan Taeyoung <you@example.com>
2018-12-15 16:41:13 -0500

    some slack light stuff workin

--- app/behavior/change_outdoor_lights.js
+++ app/behavior/change_outdoor_lights.js
@@ -1,31 +1,112 @@
 var convert = require('color-convert');
 var colors = require('color-name');
+var Particle = require('particle-api-js');
+var particle = new Particle();
+
+var PRESET_RAINBOW = [0xFF0000, 0xD52A00, 0xAB5500, 0xAB7F00,
+     0xABAB00, 0x56D500, 0x00FF00, 0x00D52A,
+     0x00AB55, 0x0056AA, 0x0000FF, 0x2A00D5,
+     0x5500AB, 0x7F0081, 0xAB0055, 0xD5002B];
+
+var PRESET_PARTY = [0x5500AB, 0x84007C, 0xB5004B, 0xE5001B,
+  0xE81700, 0xB84700, 0xAB7700, 0xABAB00,
+  0xAB5500, 0xDD2200, 0xF2000E, 0xC2003E,
+  0x8F0071, 0x5F00A1, 0x2F00D0, 0x0007F9]
+
 
 module.exports = function(config, abilities) {
 
   var controller = abilities.slack.controller;
 
-  controller.hears('lights:(.*)',['direct_message', 'direct_mention', 'mention', 'ambient'], function(bot, message) {
+  controller.hears('(.*,.*)',['direct_message', 'direct_mention', 'mention', 'ambient'], function(bot, message) {
 
-      var commands = message.match[1]
-      var colors = commands
-                    .split(",")
-                    .map((d) => name_to_hex(d.trim()));
+      var commands = parse_slack_msg(message.match[1]);
 
-      var human_message = `Hi! lights lights lights! I think you requested: ${commands}, and here are them as colors: ${colors.join(", ")} `;
+      var human_message = `Let us change the lights!
+      Message format: *time period in seconds*, *colors*.
+      For example, you can say "60,random", "100, rainbow", "5, party".
+      You can also list a series of colors: "10,red,green,blue,pink", etc. (If we don't understand the colors, we'll just insert random colors.
+      You said: ${commands.duration} seconds`; //, ${commands.colors.join(", ")}`;
 
-    //The wind is blowing from ${compass} at ${d.windspeedmph}mph! Outside, it's ${d.tempf} degrees but feels like ${d.feelsLike.toFixed(2)} with a humidity of ${d.humidity}. Inside, it's ${d.tempinf} degrees. The solar radiation is ${d.solarradiation} and it's rained ${d.dailyrainin}in today so far. Hope you're having a nice day!`
+      send_message_to_spark(config, commands);
+
       bot.reply(message, human_message);
 
   });
 
 }
 
+
+
+
+function send_message_to_spark(config, commands) {
+
+  var arg = commands.duration.toString(); 
+  var fnPr = particle.callFunction({ deviceId: config.spark.device_id, name: 'setcolors', argument: arg, auth: config.spark.access_token });
+  console.log("sent message to spark! with arg: " + arg);
+  fnPr.then(function(data) {
+    console.log('Function called succesfully:', data);
+  }, function(err) {
+    console.log('An error occurred:', err);
+  });
+
+}
+
+
+
+function parse_slack_msg(msg) {
+  var commands = {};
+
+  //defaults 
+  commands.duration = 60;
+  commands.colors = "rainbow";
+  commands.human_message = "";
+
+  msgs = msg.toLowerCase().split(",");
+
+  // no commas, do default stuff
+  if (msgs.length == 1) {
+    commands.human_message += "I couldn't understand your message! reverting to rainbow mode!";
+    commands.colors = rainbow;
+    return commands;
+  }
+
+  if(!isNaN(parseFloat(msgs[0]))) {
+    commands.duration = parseFloat(msgs[0])
+  } else {
+    commands.human_message += "I couldn't understand your duration! Just gonna revert to " + commands.duration + "secs";
+  }
+ 
+  if (msgs.length == 2) {
+    switch(msgs[1]) {
+      case "rainbow": 
+        commands.colors = PRESET_RAINBOW;
+        break; 
+      case "party": 
+        commands.colors = PRESET_PARTY;
+        break; 
+      case "random": 
+        commands.colors = "random"; 
+        break; 
+      default:
+        commands.human_message += "I couldn't understand your message! just gonna do rainbow mode!";
+        commands.colors = "rainbow"; 
+    } 
+    return commands;
+  }
+
+  // custom colors
+
+   colors = msgs.slice(1).map((d) => name_to_hex(d.trim()));
+
+  return commands;
+}
+
 function name_to_hex(name) {
   if (name in colors) {
-    return "#" + convert.keyword.hex(name);
+    return convert.keyword.hex(name);
   } else {
-    return "#" + convert.rgb.hex(random_color());
+    return convert.rgb.hex(random_color());
   }
 }
 
